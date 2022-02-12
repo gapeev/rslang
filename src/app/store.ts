2c10ec85a513c841229b91833aada4f29cf3ca3a
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-02-12 23:40:05 +0300

    feat: adjust main store (add stat reducer)

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authSlice from '../features/authPage/authSlice';
 import counterReducer from '../features/counter/counterSlice';
 import audiochallengeReducer from '../features/audiochallenge/audiochallengeSlice';
+import statReducer from '../features/stat/statSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: authSlice,
     audiochallenge: audiochallengeReducer,
+    stat: statReducer,
   },
 });
 
